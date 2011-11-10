29cd1b12c0dd61f2ea0a5ce3a8c8b68246150de5
Tijs Michels <tijs@users.sourceforge.net>
2011-11-10 23:34:10 +0000

    Now palettes of different shapes and sizes can be built and displayed, selectable from the command line

--- demo/bargraph/drawPalette.py
+++ demo/bargraph/drawPalette.py
@@ -7,28 +7,33 @@
 from g2 import *
 import sys
 
-def loadPalette():
+def loadPalette(n):
     import graphsettings
-    colors = graphsettings.Colors()
+    colors = graphsettings.Colors(n)
     graph.g2_clear_palette()
     for c in colors:
         graph.g2_ink(*c)
 
-def drawPalette():
-    graph.g2_set_font_size(12)
+def drawPalette(n):
+    colorsPerRow, nrOfRows = n == 0 and (18, 14) or (n*n, n)
+    w = 468 / colorsPerRow
+    h = 336 / nrOfRows
+    graph.g2_set_font_size(n < 6 and 12 or 10)
+    so = n < 6 and 14 or 12
     c = 0
-    for y in xrange(14):
-        dh = y*24
-        for x in xrange(18):
-            dw = x*26
+    for y in xrange(nrOfRows):
+        dh = y*h
+        for x in xrange(colorsPerRow):
+            dw = x*w
             graph.g2_pen(c)
-            graph.g2_filled_rectangle(10+dw, 324-dh, 36+dw, 348-dh)
+            graph.g2_filled_rectangle(10+dw, 348-dh-h, 10+dw+w, 348-dh)
             graph.g2_pen(0) # alternatively, draw the strings in a separate loop, to switch to a black pen just once, instead of 252 times
-            graph.g2_string(14+dw, 340-dh, str(c))
+            graph.g2_string(so+dw, 340-dh, str(c))
             c += 1
 
 output = 'w' # by default, output to window only
 graph = None
+n = 0
 progName = sys.argv.pop(0)
 while sys.argv:
     arg = sys.argv.pop(0)
@@ -36,12 +41,15 @@ while sys.argv:
         output = 'f'
         graph = g2_open_PS("palette.ps", g2_A4, g2_PS_land)
         graph.g2_set_coordinate_system(0, 0, 1.725, 1.65)
+    elif arg == '-n':
+        n = int(sys.argv.pop(0))
     else:
         if arg != '-h':
             print "\n Argument '%s' is invalid." % arg
-        print '\n %s <-f> <-h>\n\n' \
-              '   -f : output to file (landscape PostScript)\n' \
-              '   -h : show this help\n\n' \
+        print '\n %s <-n n> <-f> <-h>\n\n' \
+              '   -n n : build a straightforward n * n * n palette\n' \
+              '   -f   : output to file (landscape PostScript)\n' \
+              '   -h   : show this help\n\n' \
               ' If no (valid) output device is specified, the screen is chosen.\n\n' % progName
         sys.exit(0)
 
@@ -56,8 +64,10 @@ if graph is None: # no device was specified on the command line, or the device s
     graph.g2_set_coordinate_system(0, 0, 2, 2)
 
 graph.g2_set_auto_flush(False)
-loadPalette()
-drawPalette()
+if n != 0 and n < 3:
+    n = 3
+loadPalette(n)
+drawPalette(n)
 graph.g2_flush()
 
 if output != 'f':
--- demo/bargraph/graphsettings.py
+++ demo/bargraph/graphsettings.py
@@ -28,15 +28,27 @@ def frange(from_, to_, n):
     return (q * (from_ * (m - i) + to_ * i) for i in range(n)) # generator
 
 class ColorSteps(tuple):
-    def __new__(cls):
+    def __new__(cls, n): # a succession of n intensities (0.0, ..., 1.0)
         import math
-        return tuple.__new__(cls, (math.log(a) for a in frange(1,math.e,13)))
+        return tuple.__new__(cls, (math.log(a) for a in frange(1, math.e, n)))
 
 class Colors(list):
-    def __init__(self):
-        steps = ColorSteps()
+    def allTriplets(self, values):
+        for r in values:
+            for g in values:
+                for b in values:
+                    self.append((r, g, b)) # 27, 64, 125, 216, etc. colors
 
-        c1 = (0.,) + steps[4:12:2] + (1.,)
+    def __init__(self, n = 0):
+        if n != 0:
+            self.allTriplets(ColorSteps(n < 3 and 3 or n))
+            return
+
+        # the compact yet fairly complete default palette
+        steps = ColorSteps(13)
+
+        self.allTriplets((0.,) + steps[4:13:2]) # 6 * 6 * 6 colors
+
         c2 = steps[1:7:2]
 
         u_, v_, w_, = steps[11], steps[9], 1.
@@ -45,17 +57,12 @@ class Colors(list):
               ((v_, u_, v_), (v_, u_, w_), (w_, u_, w_)),
               ((v_, v_, u_), (v_, w_, u_), (w_, w_, u_)))
 
-        for r in c1:
-            for g in c1:
-                for b in c1:
-                    self.append((r, g, b))
-
         for r, l in zip(c2, c3):
             for g in c2:
                 for b in c2:
-                    self.append((r, g, b))
+                    self.append((r, g, b)) # 3 * 3 * 3 colors
             for a in l:
-                self.append(a)
+                self.append(a) # 3 * 3 colors
 
 black_   = 0
 blue_    = 5
